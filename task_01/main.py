264a4ae8fe16a449f9cc79b9be88af5661edbc98
Jan Tonner <37844751+lalpix@users.noreply.github.com>
2022-10-11 16:00:58 +0200

    Parsing script

---
+++ task_01/main.py
@@ -0,0 +1,50 @@
+import csv
+file1 = open('C:\programovani\KTI_datapickup\data1.txt', 'r')
+
+Lines = file1.readlines()
+c=0
+header=["timestamp","cpu","mem free","disk","eth"]
+dataArr=[]
+ln=""
+f=open('countries.csv', 'w', encoding='UTF8',newline='')
+writer = csv.writer(f)
+writer.writerow(header)
+for line in Lines:
+    c=c+1
+    if line.startswith('-'):
+        print(ln)
+        if c!=1:
+            writer.writerow(dataArr)
+        dataArr = []
+        c=0
+    elif c==3:
+        arr = line.split()
+        dataArr.append(arr[0])
+        dataArr.append(arr[2])
+        ln+=arr[0]+" "
+        ln+="cpu ut: "
+        ln+=arr[2]
+    elif c==8:
+        arr = line.split()
+        dataArr.append(arr[2])
+        ln+=" mem- tot: "+arr[1]+" free: "+arr[2]
+    elif c==11:
+        arr = line.split()
+        dataArr.append(arr[2])
+        ln+=" disk: "+arr[2]
+    elif c==15:
+        arr = line.split()
+        dataArr.append(arr[2])
+        ln += " enp2: " + arr[2]
+    elif c == 16:
+        arr = line.split()
+        dataArr.append(arr[2])
+        ln += " enp3: " + arr[2] +"\n"
+
+
+
+
+
+
+
+
